a5b11b048f79beb739e0ce5d9bd98e2360742eca
sambapython@gmail.com <sambapython@gmail.com>
2019-04-18 22:44:48 +0530

    change in Docker file

--- project/shorttestpath/entry.sh
+++ project/shorttestpath/entry.sh
@@ -1,1 +1,2 @@
+pip install -r shorttestpath/requirements.txt
 python shorttestpath/manage.py runserver 0.0.0.0:8000
--- project/shorttestpath/shorttestpath/settings.py
+++ project/shorttestpath/shorttestpath/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wf*(-$(-w7xi0(z@!5x_9dvb*ui!2=&n9x5bx#926yc)c4v1js'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["192.168.99.100","*"]
 
 
 # Application definition
